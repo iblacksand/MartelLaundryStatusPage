c94bbec8483b66e15fb093d67c4bf4499f23b032
iblacksand <chess4j@gmail.com>
2019-09-19 00:02:22 -0500

    adds color blindness mode and a better example

--- js/main.js
+++ js/main.js
@@ -3,3 +3,14 @@ $('[hasemails]').each((i, el) => {
     h = h.replace("***emails***","jme10@rice.edu");
     $(el).html(h);
 })
+toggleon = true;
+function colorblind(){
+    if(toggleon){
+    $('body').css({"-webkit-filter": "grayscale(1)", "-moz-filter": "grayscale(1)", "-ms-filter":"grayscale(1)", "filter": "grayscale(1)", "color": "black"});
+    toggleon = false;
+    }
+    else{
+        $('body').removeAttr('style');
+        toggleon = true;
+    }
+}
