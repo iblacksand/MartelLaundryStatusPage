400c0aa3db9c3a172bcace74b65d72385451f2e7
iblacksand <chess4j@gmail.com>
2019-09-20 14:46:44 -0500

    adds forms and show/hide button

--- js/main.js
+++ js/main.js
@@ -14,3 +14,19 @@ function colorblind(){
         toggleon = true;
     }
 }
+
+tohidetables = true;
+
+function detailbuttonpress(){
+    console.log('Button pressed');
+    if(tohidetables){
+        tohidetables = false;
+        $("#detailbutton").html('<span class="icon is-small"><i class="fas fa-chevron-up"></i></span><span>Show Details</span>');
+        $("#tablecols").hide();
+    }
+    else{
+        tohidetables = true;
+        $("#detailbutton").html('<span class="icon is-small"><i class="fas fa-chevron-down"></i></span><span>Hide Details</span>');
+        $("#tablecols").show();
+    }
+}
