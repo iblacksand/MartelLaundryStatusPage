cffc98e9139be52423883bb38d6a6fcbfd635007
iblacksand <chess4j@gmail.com>
2020-08-25 21:24:29 -0500

    adds comments to main.js

--- js/main.js
+++ js/main.js
@@ -2,28 +2,28 @@ $('[hasemails]').each((i, el) => {
     h = $(el).html();
     h = h.replace("***emails***","jme" + "10" + "@" +"r" +"ice" + "." + "edu");
     $(el).html(h);
-})
-toggleon = true;
-function colorblind(){
+}) // replaces fields with [hasemails] with real emails so bots can't scrape emails from the website and spam it
+toggleon = true; // true if button will activate filter
+function colorblind(){ // changes the website from color to black and white
     if(toggleon){
-    $('#colorblock').css({"-webkit-filter": "grayscale(1)", "-moz-filter": "grayscale(1)", "-ms-filter":"grayscale(1)", "filter": "grayscale(1)", "color": "black"});
+    $('#colorblock').css({"-webkit-filter": "grayscale(1)", "-moz-filter": "grayscale(1)", "-ms-filter":"grayscale(1)", "filter": "grayscale(1)", "color": "black"}); // changes all of the contenst under colorblock div to be under a b&w filter
     toggleon = false;
     }
     else{
-        $('#colorblock').removeAttr('style');
+        $('#colorblock').removeAttr('style'); // remove filter from div
         toggleon = true;
     }
 }
 
-tohidetables = true;
+tohidetables = true; // to hide tables of the washer/dryer status on button press
 
 function detailbuttonpress(){
-    if(tohidetables){
-        tohidetables = false;
+    if(tohidetables){ // will hide the tables
+        tohidetables = false; 
         $("#detailbutton").html('<span class="icon is-small"><i class="fas fa-chevron-up"></i></span><span>Show Details</span>');
         $("#tablecols").hide();
     }
-    else{
+    else{ // show the tables
         tohidetables = true;
         $("#detailbutton").html('<span class="icon is-small"><i class="fas fa-chevron-down"></i></span><span>Hide Details</span>');
         $("#tablecols").show();
