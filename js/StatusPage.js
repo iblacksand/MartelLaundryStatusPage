8cadbce912b6313152d59c1566d86886f1b67d72
iblacksand <chess4j@gmail.com>
2020-08-25 21:25:26 -0500

    adds comments to StatusPage.js

--- js/StatusPage.js
+++ js/StatusPage.js
@@ -1,5 +1,5 @@
-var socket = io();
-socket.emit('amUser');
-socket.on('updateClient', (data) => {
+var socket = io(); // initialize socket
+socket.emit('amUser'); // let server know that it is a user
+socket.on('updateClient', (data) => { // base function to update tables
     
 })
